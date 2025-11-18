4041fe8743843c413f749287a6b0fb5b6f2443b3
Md Nayeem Ahmed <git-nayeem@devxhub.com>
2025-11-18 17:48:41 +0600

    fix web

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,13 @@ import 'feature/auth/ui/controller/auth_controller.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Get.put(AuthController()); // Inject controller
   runApp(const MyApp());
 }
